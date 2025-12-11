3795215a4f3b737abe6de676362957abf48689d2
Daniel Park <danielpark@Daniels-MacBook-Air.local>
2025-12-11 14:33:13 -0500

    created a function to extract video data and statistics.

--- video_stats.py
+++ video_stats.py
@@ -76,10 +76,54 @@ def get_video_ids(playlistID):
     except requests.exceptions.RequestException as e:
         raise e
 
+def extract_video_data(video_ids):
+    extracted_data = []
+
+    def batch_list(video_id_list, batch_size):
+
+        for video_id in range(0, len(video_id_list), batch_size):
+            yield video_id_list[video_id: video_id + batch_size]
+
+    try :
+        for batch in batch_list(video_ids, maxResults):
+            video_ids_str= ",".join(batch)
+
+            url = f"https://youtube.googleapis.com/youtube/v3/videos?part=contentDetails&part=snippet&part=statistics&id={video_ids_str}&key={API_KEY}"
+
+            response = requests.get(url)
+
+            response.raise_for_status()
+
+            data = response.json()
+
+            for item in data.get("items", []):
+                video_id = item['id']
+                snippet = item['snippet']
+                contentDetails = item['contentDetails']
+                statistics = item['statistics']
+
+                video_data = {
+                    "video_id": video_id,
+                    "title": snippet['title'],
+                    "publishedAt": snippet['publishedAt'],
+                    "duration": contentDetails['duration'],
+                    "viewCount": statistics.get('viewCount', None),
+                    "likeCount": statistics.get('likeCount', None),
+                    "commentCount": statistics.get('commentCount', None),
+                }
+
+                extracted_data.append(video_data)
+
+        return extracted_data
+
+    except requests.exceptions.RequestException as e:
+        raise e
+
 
 # Test the function, where if run directly; __name__ will be set to "__main__"
     # however, if this file is imported as a module in another file, __name__ will be set to the module's name
 if __name__ == "__main__":
     playlistID = get_playlist_id()
-    print(get_video_ids(playlistID))
+    video_ids = get_video_ids(playlistID)
+    extract_video_data(video_ids)
 
