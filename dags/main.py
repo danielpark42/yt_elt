bffc1ac6cfad35eaef429ac88668a47c7f6ac196
Daniel Park <danielpark@Daniels-MacBook-Air.local>
2025-12-29 18:09:08 -0500

    updated the dag structure to run after each other, and updated the unit test to account for the new trigger tasks added to our dags

--- dags/main.py
+++ dags/main.py
@@ -1,6 +1,7 @@
 from airflow import DAG
 import pendulum 
 from datetime import datetime, timedelta
+from airflow.operators.trigger_dagrun import TriggerDagRunOperator
 
 from api.video_stats import (
     get_playlist_id, 
@@ -49,13 +50,13 @@ with DAG(
     extract_data = extract_video_data(video_ids)
     save_to_json_task = save_to_json(extract_data)
 
-    # trigger_update_db = TriggerDagRunOperator(
-    #     task_id="trigger_update_db",
-    #     trigger_dag_id="update_db",
-    # )
+    trigger_update_db = TriggerDagRunOperator(
+        task_id="trigger_update_db",
+        trigger_dag_id="update_db",
+    )
 
     # Define dependencies
-    playlist_id >> video_ids >> extract_data >> save_to_json_task
+    playlist_id >> video_ids >> extract_data >> save_to_json_task >> trigger_update_db
 
 # DAG 2: update_db
 with DAG(
@@ -70,13 +71,13 @@ with DAG(
     update_staging = staging_table()
     update_core = core_table()
 
-    # trigger_data_quality = TriggerDagRunOperator(
-    #     task_id="trigger_data_quality",
-    #     trigger_dag_id="data_quality",
-    # )
+    trigger_data_quality = TriggerDagRunOperator(
+        task_id="trigger_data_quality",
+        trigger_dag_id="data_quality",
+    )
 
     # Define dependencies
-    update_staging >> update_core
+    update_staging >> update_core >> trigger_data_quality
 
 # DAG 3: data_quality
 with DAG(
